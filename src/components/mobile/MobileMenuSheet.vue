2c85a9144656772d6ebcce1664cd4b9d021b198f
Kirill Katz <xnlyre@protonmail.com>
2026-05-24 17:45:30 +0300

    Update

--- src/components/mobile/MobileMenuSheet.vue
+++ src/components/mobile/MobileMenuSheet.vue
@@ -8,7 +8,7 @@ defineProps<{
 </script>
 
 <template>
-  <MobileBottomSheet :show="app.menuSheetOpen" z-class="z-[80]" @close="app.closeSheets">
+  <MobileBottomSheet :show="app.menuSheetOpen" z-class="z-[80]" min-height-class="min-h-0" @close="app.closeSheets">
     <div class="grid gap-2">
       <button class="inline-flex min-h-12 items-center justify-start rounded-xl border-0 bg-panel px-4 text-left font-normal text-ink transition hover:bg-white/5 active:scale-[0.99]" type="button" @click="app.openTimeline('replace')">Timeline</button>
       <button class="inline-flex min-h-12 items-center justify-start rounded-xl border-0 bg-panel px-4 text-left font-normal text-ink transition hover:bg-white/5 active:scale-[0.99]" type="button" @click="app.openReports('replace')">Reports</button>
